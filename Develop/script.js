3a8432226f43ae420a0c0b376f78429ccc6f77be
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-06-05 20:45:39 +1000

    Update script.js
    Added User prompts and function for randomization of symbols

--- Develop/script.js
+++ Develop/script.js
@@ -7,11 +7,19 @@ var generateBtn = document.querySelector("#generate");
 
 
   //  1. Prompt the user for the password criteria
-  var pslength=prompt ("Enter password length! Note: Min. 8 - Max 128 ")
+  var psLength=prompt ("Enter password length! Note: Min. 8 - Max 128 ")
   //    a. Password lengths 8 to 128
+  var psspecify=prompt ("Should password include any lowercases, uppercases, numbers and special characters? Y/N")
   // 	  b. Lowercase, uppercase, numbers and special characters
+
   //  2. Validate the input 
+  
   //  3. Generate password based on criteria 
+  
+  function getRandomSymbol() {
+    const symbols = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
 
   //  4. Display password to the page 
 
