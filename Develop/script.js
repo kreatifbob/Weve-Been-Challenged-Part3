f189c58cea1ec6f5647f8d0f1d9d4e291ade89c6
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-06-05 23:02:24 +1000

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -3,27 +3,38 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-  console.log("Hey you clicked on me")
 
 
   //  1. Prompt the user for the password criteria
-  var psLength=prompt ("Enter password length! Note: Min. 8 - Max 128 ")
+  var psLengthVar=prompt ("Enter password length! Note: Min. 8 - Max 128 ");
   //    a. Password lengths 8 to 128
-  var psspecify=prompt ("Should password include any lowercases, uppercases, numbers and special characters? Y/N")
+  var psSpecifyVar=prompt ("Should password include any lowercases, uppercases, numbers and special characters? Y/N")
   // 	  b. Lowercase, uppercase, numbers and special characters
 
   //  2. Validate the input 
+  // Unable to login into canvas and watch recordings of classes have emailed support multiple times and received no response
+
   
   //  3. Generate password based on criteria 
-  
-  function getRandomSymbol() {
-    const symbols = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
-    return symbols[Math.floor(Math.random() * symbols.length)];
+    function generatePassword(length = 8) {
+    let password = '';
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!#$%&()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let passwordLength = length;
+   
+    let array = [];
+    for (let i=0; i<length; i++){
+      array.push(Math.floor(Math.random() * chars.length)); 
+    }
+   
+    for (let i = 0; i < passwordLength; i++) {
+      password += chars[array[i]]; 
+    }
+
+    return password;
   }
 
-  //  4. Display password to the page 
 
-// Write password to the #password input
+  //  4. Display password to the page 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -34,3 +45,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
