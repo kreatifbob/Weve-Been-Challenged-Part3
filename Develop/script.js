7683f54185f45feddeacb72eee13e47c90063123
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-06-05 19:15:41 +1000

    Update script.js
    Added comments and guidelines to follow for the code which is to be refracted

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,16 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+  console.log{"Hey you clicked on me"}
+
+//  1. Prompt the user for the password criteria
+//    a. Password lengths 8 to 128
+// 	  b. Lowercase, uppercase, numbers and special characters
+//  2. Validate the input 
+//  3. Generate password based on criteria 
+
+//  4. Display password to the page 
+	return "Generated pass will go here";
 
 // Write password to the #password input
 function writePassword() {
