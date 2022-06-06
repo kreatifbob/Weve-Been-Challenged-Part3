e2dca72da60b5fec8881a7510dc3ff95797ed738
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-06-06 22:02:02 +1000

    Update script.js
    Update function, next step is to remove prompts and put in confirms

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+function masterPassword() {
+  var generateBtn = document.querySelector("#generate");
 
 
   //  1. Prompt the user for the password criteria
   var psLengthVar=prompt ("Enter password length! Note: Min. 8 - Max 128 ");
   //    a. Password lengths 8 to 128
-  var psSpecifyVar=prompt ("Should password include any lowercases, uppercases, numbers and special characters? Y/N")
+  var psSpecifyVar=prompt ("Should password include any lowercases")
   // 	  b. Lowercase, uppercase, numbers and special characters
-
+  // , uppercases, numbers and special characters?
   //  2. Validate the input 
-  // Unable to login into canvas and watch recordings of classes have emailed support multiple times and received no response
+  if (psLengthVar<8 || psLengthVar>128) {
+    alert("Please select a number between 8 and 128 and try again")
+    masterPassword();
+  }
+    // Unable to login into canvas and watch recordings of classes have emailed support multiple times and received no response
 
   
   //  3. Generate password based on criteria 
@@ -32,6 +37,9 @@ var generateBtn = document.querySelector("#generate");
 
     return password;
   }
+}
+masterPassword();
+
 
 
   //  4. Display password to the page 
