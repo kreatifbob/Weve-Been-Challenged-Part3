b9b870ae094977d97095105ccac043021d00a726
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-06-06 22:36:22 +1000

    Update script.js

--- script.js
+++ script.js
@@ -7,24 +7,32 @@ function masterPassword() {
 
 
   //  1. Prompt the user for the password criteria
-  var psLengthVar=prompt ("Enter password length! Note: Min. 8 - Max 128 ");
   //    a. Password lengths 8 to 128
-  var psSpecifyVar=prompt ("Should password include any lowercases")
-  // 	  b. Lowercase, uppercase, numbers and special characters
-  // , uppercases, numbers and special characters?
+  var psLengthVar=confirm ("Enter password length! Note: Min. 8 - Max 128 ");
+  // 	  b. Lowercase, 
+  var psSpecifyVarLower=confirm ("Should password include any lowercases")
+  //    c. Uppercase, 
+  var psSpecifyVarUpper=confirm ("Should password include any uppercases")
+  //    d. Numbers 
+  var psSpecifyVarNumbers=confirm ("Should password include any numbers")
+  //    e. Special characters
+  var psSpecifyVarSpecial=confirm ("Should password include any numbers")
   //  2. Validate the input 
+  
   if (psLengthVar<8 || psLengthVar>128) {
-    alert("Please select a number between 8 and 128 and try again")
     masterPassword();
-  }
-    // Unable to login into canvas and watch recordings of classes have emailed support multiple times and received no response
+   }
+  
 
   
-  //  3. Generate password based on criteria 
+    //  3. Generate password based on criteria 
     function generatePassword(length = 8) {
     let password = '';
-    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!#$%&()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let passwordLength = length;
+    let psSpecifyVarLower = "abcdefghijklmnopqrstuvwxyz";
+    let psSpecifyVarUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let psSpecifyVarNumbers = "0123456789";
+    let psSpecifyVarSpecial = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+    let psLengthVar = length;
    
     let array = [];
     for (let i=0; i<length; i++){
@@ -36,14 +44,14 @@ function masterPassword() {
     }
 
     return password;
+   }
   }
-}
-masterPassword();
+ masterPassword();
 
 
 
   //  4. Display password to the page 
-function writePassword() {
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
